87d8d88fbba6d1b0e4a3a176b817b51b9a601458
Guilherme G S <contatoguilhermegsz@gmail.com>
2024-11-23 20:52:48 -0300

    adicionando registerControllers a classe de server

--- src/server/index.ts
+++ src/server/index.ts
@@ -4,10 +4,10 @@ import { SERVER } from '../config';
 import {  injectable } from 'inversify'
 import express, { Express } from 'express';
 import { loadModules } from '../utils/loadModules';
-import { registerControllers } from '../utils/registerControllers';
+import { RouteHandler } from '../@types/route';
 import { errorHandler } from './middlewares/errorHandler';
 import AppContainer from '../config/container';
-import HealthCheckService from '../infra/service/health';
+import { Container } from 'inversify';
 
 @injectable()
 class Server {
@@ -20,11 +20,8 @@ class Server {
 
     public async start(): Promise<void> {
         this.app.use(express.json());
-        
-        const container = await AppContainer.getInstance();
 
-        const controllers = await loadModules('src/server/controllers');
-        registerControllers(controllers, this.app, container);
+        this.registerControllers(await loadModules('src/server/controllers'));
 
         this.app.use(errorHandler);
         this.server = http.createServer(this.app);
@@ -41,6 +38,27 @@ class Server {
     private closeServer(): void {
         if (this.server) this.server.close();
     }
+
+    private async registerControllers(controllers: any[]) {
+        const container = await AppContainer.getInstance();
+
+        controllers.forEach((ControllerClass) => {
+            const controller = container.get(ControllerClass) as any;
+        
+            const prefixRouter = Reflect.getMetadata('prefixRouter', ControllerClass) as string || '';
+            const routerHandlers = Reflect.getMetadata('routerHandlers', controller) as RouteHandler;
+
+            if (!routerHandlers) return;
+
+            Array.from(routerHandlers.entries()).forEach(([method, routes]) => {
+                Array.from(routes.entries()).forEach(([route, handlers]) => {
+                    if (handlers) {
+                        this.app[method as keyof Express](prefixRouter + route, handlers);
+                    }
+                });
+            });
+    });
+}
 }
 
 export default Server;
